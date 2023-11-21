d84f9ba0f4d09687473a8253f8fc5f089d7f949b
dhanupri <81618507+dhanupri@users.noreply.github.com>
2023-11-22 00:17:31 +0530

    Update UC1.java

--- UC1.java
+++ UC1.java
@@ -4,8 +4,8 @@ class UC1
 	public static void main(String[] args)
 	{
 	  System.out.println("Welcome to Employee Wage Computation");
-		String arr1={"0","0"};
-		UC2.main(arr1);
+		
+		int present_or_absent=UC2.presentOrAbsent();
 		
 	
 	}
